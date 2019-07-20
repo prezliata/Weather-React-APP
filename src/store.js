bcfb6b434ae5a0b9c30cabc08e108ac1f97736fd
Taras <taras.prezlyata.sign.in@gmail.com>
2019-07-20 21:10:02 +0300

    added store js

---
+++ src/store.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+export default class Store extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			appName: 'Weather app'
+		};
+	}
+
+	render() {
+		return React.Children.map(this.props.children, (child) => {
+			return React.cloneElement(child, { ...this.state });
+		});
+	}
+}
